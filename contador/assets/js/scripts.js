d20f0ef8b95d60a87093c9228db0b10e93c9a5d5
cVitorAndrade <vitorandrade1804@gmail.com>
2022-06-17 21:09:52 -0300

    conuter max value

--- contador/assets/js/scripts.js
+++ contador/assets/js/scripts.js
@@ -2,13 +2,23 @@ var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
 function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber < 50)
+    {
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }else{
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
     check();
 }
 function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber > -50)
+    {
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }else{
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
     check();
 }
 
